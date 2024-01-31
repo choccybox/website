331aaaff2ea5f5640dd5b5a3c6b9089c9089be28
chocsteryu <99021003+choccymilku@users.noreply.github.com>
2024-01-31 18:04:50 +0100

    yea

--- api/player.js
+++ api/player.js
@@ -11,6 +11,8 @@ const spotifyClientSecret = process.env.SPOTIFY_CLIENT_SECRET;
 const spotifyRedirectUri = `http://localhost:3000/playercallback`;
 const spotifyScopes = 'user-read-currently-playing user-library-read user-read-recently-played user-top-read user-read-playback-state';
 const spotifyTokenFile = 'spotify.json';
+const TOKEN_FILE = 'soundcloud.json';
+
 
 // spotify tokens
 try {
@@ -37,6 +39,34 @@ function saveSpotifyTokensToFile() {
     console.log('spotify token saved');
   } catch (err) {
     console.error('error saving spotify token:', err.message);
+  }
+}
+
+async function saveSoundcloudToken(soundAccessToken, soundRefreshToken) {
+  try {
+    await fs.writeFile(TOKEN_FILE, JSON.stringify({ access_token: soundAccessToken, refresh_token: soundRefreshToken }));
+    console.log('SoundCloud tokens saved successfully.');
+    console.log({ soundAccessToken, soundRefreshToken });
+    console.log(await fs.readFile(TOKEN_FILE, 'utf8'));
+  } catch (error) {
+    console.error('Error saving SoundCloud tokens:', error);
+  }
+}
+
+// Function to load the access token and refresh token from a JSON file
+async function loadSoundcloudToken() {
+  try {
+    const data = await fs.readFile(TOKEN_FILE);
+    const { access_token: soundAccessToken, refresh_token: soundRefreshToken } = JSON.parse(data);
+    console.log('SoundCloud tokens loaded successfully.');
+    // log what was loaded from the file
+    console.log({ soundAccessToken, soundRefreshToken });
+    // log how the file looks like
+    console.log(await fs.readFile(TOKEN_FILE, 'utf8'));
+    return { soundAccessToken, soundRefreshToken };
+  } catch (error) {
+    console.error('Error loading SoundCloud tokens:', error);
+    return null;
   }
 }
 
@@ -82,6 +112,47 @@ player.get('/playercallback', async (req, res) => {
     }, 2000);
   }
 }); */
+
+player.get('/soundauth', (req, res) => {
+  // Redirect the user to the SoundCloud authorization URL
+  const authorizeUrl = `https://soundcloud.com/connect?client_id=${process.env.SOUNDCLOUD_CLIENT_ID}&redirect_uri=${process.env.SOUNDCLOUD_REDIRECT_URI}&response_type=code&scope=non-expiring`;
+  res.redirect(authorizeUrl);
+});
+
+player.get('/soundcallback', async (req, res) => {
+  // Handle the callback after the user grants/denies authorization
+  const { code } = req.query;
+
+  // Exchange the authorization code for an access token
+  const tokenUrl = 'https://api.soundcloud.com/oauth2/token';
+  const params = new URLSearchParams({
+    client_id: process.env.SOUNDCLOUD_CLIENT_ID,
+    client_secret: process.env.SOUNDCLOUD_CLIENT_SECRET,
+    redirect_uri: process.env.SOUNDCLOUD_REDIRECT_URI,
+    grant_type: 'authorization_code',
+    code,
+  });
+
+  try {
+    const response = await axios.post(tokenUrl, params);
+    const accessToken = response.data.access_token;
+    const refreshToken = response.data.refresh_token;
+
+    // Store the access token and refresh token in sound_token.json
+    await saveSoundcloudToken(accessToken, refreshToken);
+
+    // log what is saved
+    console.log({ accessToken, refreshToken });
+    // log how the file looks like
+    console.log(await fs.readFile(TOKEN_FILE, 'utf8'));
+
+    // Display user data in JSON format
+    res.json('success');
+  } catch (error) {
+    console.error('Error exchanging code for token:', error.message);
+    res.status(500).send('Error during authentication');
+  }
+});
 // COMMENT OUT AFTER LOGGING. (so some random doesnt log in)
 
 player.get('/player', async (req, res) => {
@@ -91,6 +162,61 @@ player.get('/player', async (req, res) => {
   } catch (error) {
     console.error('Error:', error.response ? error.response.data : error.message);
     res.status(error.response ? error.response.status : 500).send('Error occurred while fetching currently playing track.');
+  }
+});
+
+player.get('/sound-search/:trackname/:artistname?', async (req, res) => {
+  try {
+    // Read the stored access token and refresh token from sound_token.json
+    const tokens = await loadSoundcloudToken();
+
+    if (!tokens || !tokens.accessToken) {
+      return res.status(401).send('Access token not found. Please authorize.');
+    }
+
+    const accessToken = tokens.accessToken;
+
+    // Extract parameters from the request URL
+    const { trackname, artistname } = req.params;
+
+    // Construct the search URL using the provided trackname and artistname if available
+    let searchUrl;
+    if (artistname) {
+      searchUrl = `https://api.soundcloud.com/tracks?q=${trackname} ${artistname}&limit=1&linked_partitioning=true`;
+    } else {
+      searchUrl = `https://api.soundcloud.com/tracks?q=${trackname}&limit=1&linked_partitioning=true`;
+    }
+
+    // Make a request to the search endpoint
+    const searchResponse = await axios.get(searchUrl, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    // Check if the response contains a 'collection' property
+    if (searchResponse.data.collection) {
+      // simplify the data
+      const tracks = searchResponse.data.collection.map(track => {
+        return {
+          name: track.title,
+          artist: track.user.username,
+          url: track.permalink_url,
+          // replace the default artwork with a larger image and jpg
+          art: track.artwork_url ? track.artwork_url.replace('-large', '-t300x300') : null,
+        };
+      });
+
+      // Display search results in JSON format
+      res.json(tracks);
+    } else {
+      // Handle the case where the response format is unexpected
+      console.error('Unexpected response format:', searchResponse.data);
+      res.status(500).send('Unexpected response format');
+    }
+  } catch (error) {
+    console.error('Error searching tracks:', error.message);
+    res.status(500).send('Error searching tracks');
   }
 });
 
--- api/soundcloud.js
+++ api/soundcloud.js
@@ -8,29 +8,28 @@ const PORT = 20003;
 const TOKEN_FILE = 'soundcloud.json';
 
 // Function to save the access token and refresh token to a JSON file
-async function saveSoundcloudToken(accessToken, refreshToken) {
+async function saveSoundcloudToken(soundAccessToken, soundRefreshToken) {
   try {
-    await fs.writeFile(TOKEN_FILE, JSON.stringify({ access_token: accessToken, refresh_token: refreshToken }));
+    await fs.writeFile(TOKEN_FILE, JSON.stringify({ access_token: soundAccessToken, refresh_token: soundRefreshToken }));
     console.log('SoundCloud tokens saved successfully.');
-    console.log({ accessToken, refreshToken });
+    console.log({ soundAccessToken, soundRefreshToken });
     console.log(await fs.readFile(TOKEN_FILE, 'utf8'));
   } catch (error) {
     console.error('Error saving SoundCloud tokens:', error);
   }
 }
 
-
 // Function to load the access token and refresh token from a JSON file
 async function loadSoundcloudToken() {
   try {
     const data = await fs.readFile(TOKEN_FILE);
-    const { access_token: accessToken, refresh_token: refreshToken } = JSON.parse(data);
+    const { access_token: soundAccessToken, refresh_token: soundRefreshToken } = JSON.parse(data);
     console.log('SoundCloud tokens loaded successfully.');
     // log what was loaded from the file
-    console.log({ accessToken, refreshToken });
+    console.log({ soundAccessToken, soundRefreshToken });
     // log how the file looks like
     console.log(await fs.readFile(TOKEN_FILE, 'utf8'));
-    return { accessToken, refreshToken };
+    return { soundAccessToken, soundRefreshToken };
   } catch (error) {
     console.error('Error loading SoundCloud tokens:', error);
     return null;
@@ -115,7 +114,7 @@ soundcloud.get('/sound-search/:trackname/:artistname?', async (req, res) => {
           name: track.title,
           artist: track.user.username,
           url: track.permalink_url,
-          // replace the default artwork with a larger image
+          // replace the default artwork with a larger image and jpg
           art: track.artwork_url ? track.artwork_url.replace('-large', '-t300x300') : null,
         };
       });
