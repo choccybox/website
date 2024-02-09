e81cc5c32bed0820e345e68739b1f5cecec0fea6
chocsteryu <99021003+choccymilku@users.noreply.github.com>
2024-02-09 09:50:36 +0100

    oop

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.static(__dirname));
 
 app.use(player);
-app.user(userinfo);
+app.use(userinfo);
 
 app.listen(port, "0.0.0.0", function () {
     console.log('Server started on port ' + port);
